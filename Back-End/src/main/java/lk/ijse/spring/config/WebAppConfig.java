61b8371a3e5ced8633586aeed3c73ce67576e126
dilshan27 <damithaadilshan@gmail.com>
2023-02-15 19:47:40 +0530

    backend update

--- Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Back-End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,14 +1,32 @@
 package lk.ijse.spring.config;
 
-import lk.ijse.spring.advicer.AppWideExceptionHandler;
-import lk.ijse.spring.controller.fileUploadController;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan(basePackageClasses = {fileUploadController.class, AppWideExceptionHandler.class})
 @EnableWebMvc
-public class WebAppConfig {
+@ComponentScan(basePackages = "lk.ijse.spring")
+public class WebAppConfig implements WebMvcConfigurer {
 
+    /*
+     * First and foremost we need to configure MultipartResolver
+     */
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    /*
+     * You have to override this method and allocate the url and location for uploaded resources
+     * */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
 }
